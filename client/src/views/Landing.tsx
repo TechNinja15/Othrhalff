239c325cab8e0813e8781ada773b8d17cd28d969
Test User <test@test.com>
2026-06-08 01:06:07 +0530

    chore: fix noise.svg 404 by using inline SVG data URI

--- client/src/views/Landing.tsx
+++ client/src/views/Landing.tsx
@@ -130,7 +130,7 @@ export const Landing: React.FC = () => {
       </div>
 
       {/* Subtle noise overlay */}
-      <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-10 mix-blend-overlay pointer-events-none z-[1]"></div>
+      <div className="absolute inset-0 opacity-10 mix-blend-overlay pointer-events-none z-[1]" style={{ backgroundImage: 'url("data:image/svg+xml,%3Csvg viewBox=\'0 0 256 256\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cfilter id=\'noise\'%3E%3CfeTurbulence type=\'fractalNoise\' baseFrequency=\'0.9\' numOctaves=\'4\' stitchTiles=\'stitch\'/%3E%3C/filter%3E%3Crect width=\'100%25\' height=\'100%25\' filter=\'url(%23noise)\'/%3E%3C/svg%3E")' }}></div>
       
       {/* Premium Cinematic Background Gradients */}
       <div className="fixed inset-0 pointer-events-none z-[0] overflow-hidden">
